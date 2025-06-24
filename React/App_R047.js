2a3d15510028f8d4dad832f8b4098144ebbb93e5
jhleekr2 <jhleekr2@hanmail.net>
2025-06-25 01:45:47 +0900

    20250625

---
+++ React/App_R047.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListGroup from './R047_ReactstrapListGroup'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <p>CSS 적용하기</p>
+      <ListGroup />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/App_R063.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import R063_CallbackFunc from "./R063_CallbackFunc";
+
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <R063_CallbackFunc />
+    </div>
+  );
+}
+
+export default App;
